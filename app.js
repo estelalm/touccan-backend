7f10ea98f7a0a9275bb6f63ea72ececac452ca98
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-09-03 12:25:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Autores: Estela Alves, Gustavo de Campos, Nathalia Kawakami
+ * Data: 03 set. 2024
+ * Versão: 1.0
+ * Objetivo: Criação de uma API para manipular dados de uma aplicação de serviços autônomos.
+ */
+
+
+
+
