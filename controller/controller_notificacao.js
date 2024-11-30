2adb5ed8d9d2e60cb2fd90c6fd933400f3ae5579
estelalm <estela.almoraes@gmail.com>
2024-11-30 12:19:11 -0300

    30 nov. 2024 - firebase

---
+++ controller/controller_notificacao.js
@@ -0,0 +1,23 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert(require("./path/to/your-service-account.json")),
+});
+
+function sendNotificationToUser(fcmToken, title, body) {
+  const message = {
+    token: fcmToken,
+    notification: {
+      title: title,
+      body: body,
+    },
+  };
+
+  admin.messaging().send(message)
+    .then((response) => {
+      console.log("Notificação enviada com sucesso:", response);
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar notificação:", error);
+    });
+}
